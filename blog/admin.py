44d80e0e822740384a07f0d12d7307808b54f401
guilhermegouw <guilherme.gouw@gmail.com>
2021-04-27 17:53:33 -0300

    comment system implemented.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -13,7 +13,7 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ("status", "publish")
 
 
-@admin.register(Post)
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "post", "created", "active")
     list_filter = ("active", "created", "updated")
